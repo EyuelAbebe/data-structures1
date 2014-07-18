c7e9b04085e2a99a5374779a90ca0ca3d3d752f8
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-18 16:16:03 -0700

    Modified merge_sort function.

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,3 +1,18 @@
+def time_it(method):
+    import time
+
+    def timed(a_list):
+        time_start = time.time()
+        result = method(a_list)
+        time_end = time.time()
+
+        list_length = len(a_list)
+        print '%r Sorted_list_length: %r in %2.5f sec'%(method.__name__, list_length, time_end - time_start)
+
+        return result
+    return timed
+
+
 def mergeLnR(l_list, r_list):
     result = []
     i,j = 0, 0
@@ -15,7 +30,7 @@ def mergeLnR(l_list, r_list):
 
     return result
 
-
+@time_it
 def merge_sort(unsorted_list):
     if len(unsorted_list) < 2:
         return unsorted_list
