0f970863827956170d04083042f8374c7bbb2773
eyueld-codefellow <eyueld-codfefellow@gmail.com>
2014-07-18 16:36:30 -0700

    Created test for quick_sort

--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -7,7 +7,7 @@ def time_it(method):
         time_end = time.time()
 
         list_length = len(a_list)
-        print '%r Sorted_list_length: %r in %2.2f sec' %(method.__name__, list_length, time_end - time_start)
+        print '%r Sorted_list_length: %r in %2.5f sec' %(method.__name__, list_length, time_end - time_start)
         
         return result
     
@@ -19,5 +19,33 @@ def quick_sort(unsorted_list):
     if len(unsorted_list) < 2 :
         return unsorted_list
     else:
-        pass
+        import random
+        less, equal, greater = [],[],[]
+
+        pivot = random.choice(unsorted_list)
+
+        for element in unsorted_list:
+            if element < pivot:
+                less.append(element)
+            elif element == pivot:
+                equal.append(element)
+            else:
+                greater.append(element)
+        
+        quick_sort(less)
+        quick_sort(greater)
+
+        return less + equal + greater
+
+
+if __name__ == '__main__':
+    from random import randint
+
+    random_list = []
+    for i in range(100):
+        a = [randint(1,100) for x in range(10*(i+1))]
+        random_list.append(a)
+
+    for a_list in random_list:
+        quick_sort(a_list)
 
